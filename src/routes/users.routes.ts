5f0fcdd6abd5bd709899cfac3c5e05021f0ab46d
Wilson Alves da Silva <42125277+willrockies@users.noreply.github.com>
2023-05-08 22:18:41 -0300

    cadastro de usuarios

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (error) {
+
+    let message;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+
+    return response.status(400).json({ error: message })
+  }
+
+});
+
+export default usersRouter;
